aede396d014b37c2b5fdb28742bbefec77f88f78
Adrian Acosta <aa@ismeta.net>
2020-05-21 22:28:27 -0600

    Refactors render

--- src/components/User/Nabos.js
+++ src/components/User/Nabos.js
@@ -18,7 +18,6 @@ function Nabos(props) {
     const refreshNabos = (event,idx) => {
         console.log(`Valor precambio: ${precio} y indice ${idx}`)
         precio[idx]=event.target.value;
-        console.log(event.target);
         console.log(`Cambiado a: ${precio} y indice ${idx}`)
     }
 
@@ -32,7 +31,6 @@ function Nabos(props) {
             for (let b = 0; b < 2; b++) {
                 cols.push(<div className="col">
                     <input id={id} type="text" className="form-control" defaultValue={props.d.precios[id]} onChange={(e)=>{refreshNabos(e,e.target.id)}}/></div>)
-                console.log(id)
                 id++
             }
             form.push(<div className="form-row">{cols}</div>)
@@ -49,54 +47,6 @@ function Nabos(props) {
                     </div>
                 </div>
                 {formRender()}
-                {/* <div className="form-row">
-                    <div className="col">
-                        <input type="text" className="form-control" defaultValue={props.d.precios[1]} onChange={(e)=>{refreshNabos(e,1)}}/>
-                    </div>
-                    <div className="col">
-                        <input type="text" className="form-control" defaultValue={props.d.precios[2]} onChange={(e)=>{refreshNabos(e,2)}}/>
-                    </div>
-                </div>
-                <div className="form-row">
-                    <div className="col">
-                        <input type="text" className="form-control" defaultValue={props.d.precios[3]} onChange={(e)=>{refreshNabos(e,3)}}/>
-                    </div>
-                    <div className="col">
-                        <input type="text" className="form-control" defaultValue={props.d.precios[4]} onChange={(e)=>{refreshNabos(e,4)}}/>
-                    </div>
-                </div>
-                <div className="form-row">
-                    <div className="col">
-                        <input type="text" className="form-control" defaultValue={props.d.precios[5]} onChange={(e)=>{refreshNabos(e,5)}}/>
-                    </div>
-                    <div className="col">
-                        <input type="text" className="form-control" defaultValue={props.d.precios[6]} onChange={(e)=>{refreshNabos(e,6)}}/>
-                    </div>
-                </div>
-                <div className="form-row">
-                    <div className="col">
-                        <input type="text" className="form-control" defaultValue={props.d.precios[7]} onChange={(e)=>{refreshNabos(e,7)}}/>
-                    </div>
-                    <div className="col">
-                        <input type="text" className="form-control" defaultValue={props.d.precios[8]} onChange={(e)=>{refreshNabos(e,8)}}/>
-                    </div>
-                </div>
-                <div className="form-row">
-                    <div className="col">
-                        <input type="text" className="form-control" defaultValue={props.d.precios[9]} onChange={(e)=>{refreshNabos(e,9)}}/>
-                    </div>
-                    <div className="col">
-                        <input type="text" className="form-control" defaultValue={props.d.precios[10]} onChange={(e)=>{refreshNabos(e,10)}}/>
-                    </div>
-                </div>
-                <div className="form-row">
-                    <div className="col">
-                        <input type="text" className="form-control" defaultValue={props.d.precios[11]} onChange={(e)=>{refreshNabos(e,11)}}/>
-                    </div>
-                    <div className="col">
-                        <input type="text" className="form-control" defaultValue={props.d.precios[12]} onChange={(e)=>{refreshNabos(e,12)}}/>
-                    </div>
-                </div> */}
                 <br/>
                 <a href={"#"} className="btn btn-primary" onClick={handleChange}>Calcular</a>
             </form>
